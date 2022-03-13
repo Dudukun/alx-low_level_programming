612331698fc7fd817552443490943c9395020ba5
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-03-13 12:45:09 -0700

    Positive anything is better than negat ive nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,6 @@
 #include <time.h>
 
 /* more headers goes there */
-
-
-
 /**
  *
  * * main - entry point
@@ -24,12 +21,13 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2; /* your code goes there */
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
 
 if (n > 0)
 {
 printf("%d is positive\n", n);
-} 
+}
 
 else if (n == 0)
 {
