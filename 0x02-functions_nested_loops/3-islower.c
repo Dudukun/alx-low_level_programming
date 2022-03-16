c112e648ff6f9174db9b3da211a8250a0d4642fd
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-03-16 10:48:09 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,19 @@
 #include "main.h"
+
 /**
  * _islower - a function that checks for lowercase character
- 
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int 1)
+
 {
-if (1 >='a' && 1 < 'z')
+
+if (1 >= 'a' && c < 'z')
+
 return (1);
+
 else
 return (0);
 }
+
