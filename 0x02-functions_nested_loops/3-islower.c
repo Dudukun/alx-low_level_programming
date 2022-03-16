8287296159736b88b8ba99d158e22bdba85b6441
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-03-16 11:41:22 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,35 +11,13 @@ int _islower(int c)
 if (c >= 'a' && c <= 'z')
 {			
 return (1);
-putchar(r + '0');
 }
 else
 {
 return (0);
 }
-}
-/**
- * test_islower - Test the _islower function
- *
- * @n: Number to pass to _islower function
- */
-void test_islower(int n)
-{
-int r;
-
-r = _islower(n);
-putchar(r + '0');
+putchar(c + '0');
 putchar('\n');
 }
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-test_islower('H');
-return (0);
-}
 
