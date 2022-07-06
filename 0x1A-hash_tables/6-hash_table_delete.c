bbba8630ac550c2fa42b71be335d193033f98da3
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 00:33:15 -0700

    my comms

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,51 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "hash_tables.h"
-
 /**
-  * hash_table_delete - Deletes a hash table
-  * @ht: The hash table to frees
-  *
-  * Return: Nothing!
-  */
+ *hash_table_delete - function that deletes a hash table
+ *
+ *@ht: is the hash table
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *node = NULL, *temp = NULL;
+hash_node_t *node, *temp;
+unsigned long int cont = 0;
 
-	if (ht && ht->size && ht->array)
-	{
-		for (; i < ht->size; ++i)
-		{
-			node = ht->array[i];
-
-			if (node)
-			{
-				if (node->next)
-				{
-					node = node->next;
-					while (node)
-					{
-						temp = node;
-						node = node->next;
-						free(temp->key);
-						free(temp->value);
-						free(temp);
-					}
-				}
-
-				node = ht->array[i];
-				if (node->key && node->value)
-				{
-					free(node->key);
-					free(node->value);
-				}
-			}
-
-			free(node);
-		}
-
-		free(ht->array);
-		free(ht);
-	}
+for (cont = 0; cont < ht->size; cont++)
+{
+node = ht->array[cont];
+while (node != NULL)
+{
+temp = node;
+node = node->next;
+free(temp->key);
+free(temp->value);
+free(temp);
+}
+}
+free(ht->array);
+free(ht);
 }
